6c4e4addb16df24620efac41c4d8a28ebe159939
E-Viviane <104867990+E-Viviane@users.noreply.github.com>
2022-08-09 22:00:23 +0100

    Update _write.c

--- _write.c
+++ _write.c
@@ -1,63 +1,32 @@
-nclude "shell.h"
-
-
+#include "shell.h"
 
 /**
- *
- *  * _write - prints a character to stdout
- *
- *   * @c: a character to print
- *
- *    * 
- *
- *     * Return: numebr of printed characters
- *
- *      */
-
-
+ * _write - prints a character to stdout
+ * @c: a character to print
+ * 
+ * Return: numebr of printed characters
+ */
 
 int _putchar(char c)
-
 {
-
-	return(write(STDIN_FILENO, &c, 1));
-
+return(write(STDIN_FILENO, &c, 1));
 }
 
-
-
 /**
+ * _printstr - prints all characters from string to stdout.
+ * @c: characters in string form
  *
- *  * _printstr - prints all characters from string to stdout.
- *
- *   * @c: characters in string form
- *
- *    *
- *
- *     * Return: No of printed characters
- *
- *      */
-
-
+ * Return: No of printed characters
+ */
 
 int _write(char *ptrstr)
-
 {
+int i = 0, overall = 0;
 
-	int i = 0, overall = 0;
-
-
-
-	while (ptrstr[i])
-
-	{
-
-		overall += _putchar(ptrstr[i]);
-
-		i++;
-
-	}
-
-	return (overall);
-
+while (ptrstr[i])
+{
+overall += _putchar(ptrstr[i]);
+i++;
+}
+return (overall);
 }
