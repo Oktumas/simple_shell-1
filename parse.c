788150676506d52734b1d604ef2db88d768b13ee
E-Viviane <104867990+E-Viviane@users.noreply.github.com>
2022-08-09 22:49:35 +0100

    Update parse.c

--- parse.c
+++ parse.c
@@ -1,101 +1,51 @@
-nclude "shell.h"
-
-
+#include "shell.h"
 
 /**
+ * _parse - split environment variable
+ * @ptrData: data structure
  *
- *  * _parse - split environment variable
- *
- *   * @ptrData: data structure
- *
- *    *
- *
- *     * Return: string array
- *
- *      */
-
+ * Return: string array
+ */
 char **_parse(server_t *ptrData)
-
 {
+char **paths;
+serverEnv_t *ptrNode;
 
-	char **paths;
+ptrNode = _getenv(ptrData->env, "PATH");
+paths = _strtow(ptrNode->value, ":", NULL);
 
-	serverEnv_t *ptrNode;
-
-
-
-	ptrNode = _getenv(ptrData->env, "PATH");
-
-	paths = _strtow(ptrNode->value, ":", NULL);
-
-
-
-	return (paths);
-
+return (paths);
 }
 
-
-
 /**
+ * _generateAbsolutePath - Generate an absolute path from environment variable
  *
- *  * _generateAbsolutePath - Generate an absolute path from environment variable
+ * @prmPath: path environment variable
+ * @prmCommandName: command name
  *
- *   *
- *
- *    * @prmPath: path environment variable
- *
- *     * @prmCommandName: command name
- *
- *      *
- *
- *       * Return: absolute path
- *
- *        */
-
+ * Return: absolute path
+ */
 char *_genAbs(char *ptrPath, char *ptrCmdName)
-
 {
+	char *tmp, *path, *absolutePath, lastCharacter;
+	int pathValueSize;
 
-		char *tmp, *path, *absolutePath, lastCharacter;
+	path = _strdup(ptrPath);
+	pathValueSize = _strlen(path);
+	lastCharacter = path[pathValueSize - 1];
 
-			int pathValueSize;
+	if (lastCharacter != '/')
+	{
+		tmp = path;
+		path = _strconcat(tmp, "/");
+		free(tmp);
+	}
 
+	tmp = "";
+	tmp = _strconcat(tmp, path);
+	absolutePath = _strconcat(tmp, ptrCmdName);
+	free(tmp);
+	free(path);
 
-
-				path = _strdup(ptrPath);
-
-					pathValueSize = _strlen(path);
-
-						lastCharacter = path[pathValueSize - 1];
-
-
-
-							if (lastCharacter != '/')
-
-									{
-
-												tmp = path;
-
-														path = _strconcat(tmp, "/");
-
-																free(tmp);
-
-																	}
-
-
-
-								tmp = "";
-
-									tmp = _strconcat(tmp, path);
-
-										absolutePath = _strconcat(tmp, ptrCmdName);
-
-											free(tmp);
-
-												free(path);
-
-
-
-													return (absolutePath);
-
+	return (absolutePath);
 }
