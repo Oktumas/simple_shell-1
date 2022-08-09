924a4fe54bbf92d6dd6d83757d795b97354e1435
E-Viviane <104867990+E-Viviane@users.noreply.github.com>
2022-08-09 22:05:44 +0100

    Update free_memory.c

--- free_memory.c
+++ free_memory.c
@@ -1,229 +1,115 @@
-nclude "shell.h"
-
-
+#include "shell.h"
 
 /**
- *
- *  * freeServerData - free all pointers 
- *
- *   * @ptrData: pointer to data stored
- *
- *    */
-
-
+ * freeServerData - free all pointers 
+ * @ptrData: pointer to data stored
+ */
 
 void freeServerData(server_t *ptrData)
-
 {
+if (ptrData == NULL)
+return;
 
-	if (ptrData == NULL)
+if (ptrData->argument != NULL)
+freeArgument(ptrData->argument);
+ptrData->argument = NULL;
 
-		return;
+if (ptrData->buff != NULL && ptrData->cmdName !=  NULL)
+free(ptrData->buff);
+ptrData->buff = NULL;
+free(ptrData->cmdName);
+ptrData->cmdName = NULL;
 
-
-
-	if (ptrData->argument != NULL)
-
-		freeArgument(ptrData->argument);
-
-	ptrData->argument = NULL;
-
-
-
-	if (ptrData->buff != NULL && ptrData->cmdName !=  NULL)
-
-		free(ptrData->buff);
-
-	ptrData->buff = NULL;
-
-	free(ptrData->cmdName);
-
-	ptrData->cmdName = NULL;
-
-
-
-	if (ptrData->cmdList != NULL && ptrData->history != NULL)
-
-		freeArgument(ptrData->cmdList);
-
-	ptrData->cmdList = NULL;
-
-	freeArgument(ptrData->history);
-
-	ptrData->history = NULL;
-
-
+if (ptrData->cmdList != NULL && ptrData->history != NULL)
+freeArgument(ptrData->cmdList);
+ptrData->cmdList = NULL;
+freeArgument(ptrData->history);
+ptrData->history = NULL;
 
 }
 
-
-
 /**
- *
- *  * freeArgument - free the memory of a double pointer
- *
- *   * @ptr: double pointer
- *
- *    */
-
-
+ * freeArgument - free the memory of a double pointer
+ * @ptr: double pointer
+ */
 
 void freeArgument(char **ptr)
-
 {
+int i;
 
-	int i;
+if (ptr == NULL)
+return;
 
-
-
-	if (ptr == NULL)
-
-		return;
-
-
-
-	for (i = 0; ptr[i] != NULL; i++)
-
-	{
-
-		free(ptr[i]);
-
-		ptr[i] = NULL;
-
-	}
-
-	free(ptr);
-
+for (i = 0; ptr[i] != NULL; i++)
+{
+free(ptr[i]);
+ptr[i] = NULL;
 }
-
-
+free(ptr);
+}
 
 void freeList(serverEnv_t *ptrHeadNode)
-
 {
+if (ptrHeadNode == NULL)
+return;
 
-	if (ptrHeadNode == NULL)
-
-		return;
-
-
-
-	freeList(ptrHeadNode->next);
-
-	free(ptrHeadNode->pathName);
-
-	ptrHeadNode->pathName = NULL;
-
-	free(ptrHeadNode->value);
-
-	ptrHeadNode->value = NULL;
-
-	free(ptrHeadNode->globPath);
-
-	ptrHeadNode->globPath = NULL;
-
-	free(ptrHeadNode);
-
+freeList(ptrHeadNode->next);
+free(ptrHeadNode->pathName);
+ptrHeadNode->pathName = NULL;
+free(ptrHeadNode->value);
+ptrHeadNode->value = NULL;
+free(ptrHeadNode->globPath);
+ptrHeadNode->globPath = NULL;
+free(ptrHeadNode);
 }
-
-
-
 
 
 /**
+ * _absolute - return absolute path of a command
+ *@ptrData: app data structure
  *
- *  * _absolute - return absolute path of a command
- *
- *   *@ptrData: app data structure
- *
- *    *
- *
- *     * Return: absolute path
- *
- *      */
-
+ * Return: absolute path
+ */
 char *_absolute(server_t *ptrData)
-
 {
+	char **pList, *abs;
+	struct stat st;
+	int iloop = 0;
 
-		char **pList, *abs;
+	if (
+		ptrData->cmdName[0] == '.' &&
+		ptrData->cmdName[1] == '/' &&
+		stat(ptrData->cmdName, &st) == 0
+	)
+		return (ptrData->cmdName);
 
-			struct stat st;
+	pList = _parse(ptrData);
 
-				int iloop = 0;
+	if (pList == NULL)
+		return (NULL);
 
+	while (pList[iloop] != NULL)
+	{
+		abs = _genAbs(pList[iloop], ptrData->cmdName);
 
+	
+		if (stat(abs, &st) == 0)
+		{
+			freeArgument(pList);
+			return (abs);
+		}
+		free(abs);
+		iloop++;
+	}
+	freeArgument(pList);
 
-					if (
+	
+	if (stat(ptrData->cmdName, &st) == 0)
+	{
+		return (ptrData->cmdName);
+	}
+	else
+		_Error(ptrData, 101);
 
-									ptrData->cmdName[0] == '.' &&
-
-											ptrData->cmdName[1] == '/' &&
-
-													stat(ptrData->cmdName, &st) == 0
-
-														)
-
-								return (ptrData->cmdName);
-
-
-
-						pList = _parse(ptrData);
-
-
-
-							if (pList == NULL)
-
-										return (NULL);
-
-
-
-								while (pList[iloop] != NULL)
-
-										{
-
-													abs = _genAbs(pList[iloop], ptrData->cmdName);
-
-
-
-														
-
-															if (stat(abs, &st) == 0)
-
-																		{
-
-																						freeArgument(pList);
-
-																									return (abs);
-
-																											}
-
-																	free(abs);
-
-																			iloop++;
-
-																				}
-
-									freeArgument(pList);
-
-
-
-										
-
-										if (stat(ptrData->cmdName, &st) == 0)
-
-												{
-
-															return (ptrData->cmdName);
-
-																}
-
-											else
-
-														_Error(ptrData, 101);
-
-
-
-												return (NULL);
-
+	return (NULL);
 }
